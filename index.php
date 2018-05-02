9355ed88b0850b88ae110a2a4e6761507d1ac161
Lazycorn <linktrainery@gmail.com>
2018-05-02 12:27:55 +0200

    hello
    first fix y'all

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
 /*
-
+//Create function for the Subpages
+- Start
++- Läsa inlägg som News
++- Kommentera på inlägg. (Krävs inlogg)
++- Gilla inläggen.
+- About
+- Posts (Syns bara för inloggade)
++- Man skall kunna posta inlägg.
++- Redigera inlägg
+- Profil
++- Redigera information.
+++- Namn, Bild, Civilstatus osv.
 Vad skall index.php bestå utav. Kolla WU2_ViBo/index.php på GitHub.
-
 */
 
  ?>
